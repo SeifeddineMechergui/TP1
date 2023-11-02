1fa6680de814905d971ec271869c5b1f381873b8
SeifeddineMechergui <saifmechergui2222@gmail.com>
2023-11-02 19:19:50 +0100

    fix bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './MonSite/toolbar/toolbar.component';
+import { FooterComponent } from './MonSite/footer/footer.component';
+import { ListComponent } from './MonSite/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    FooterComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
